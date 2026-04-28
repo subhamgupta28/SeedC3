3db81bf7d3a57524dc2228bda07b15e752dcb43c
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2026-04-28 13:35:47 +0530

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -198,10 +198,10 @@ void readPow()
 
   totalEnergy += -power_mW * (timeInterval / (60 * 60));
   capacity_mAh += current_mA * (timeInterval / (60 * 60));
-  isDischarge = c1_curr < 0 ? "DISCHARGE" : "CHARGING";
+  isDischarge = c1_curr > 0.5 ? "CHARGING" : "DISCHARGE";
   // percent = mapf(busvoltage, 12.8, 16.6, 0.0, 100.0);
   percent = 100.0 * (1.0 - (totalEnergy / MAX_ENERGY_Wh));
-  if (isDischarge == "CHARGING" && c1_curr > 0)
+  if (isDischarge == "CHARGING" && c1_curr > 0.5)
   {
     float remainingCapacity = targetCapacity - capacity_mAh;
 
